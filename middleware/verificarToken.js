97ab97579721e40f9894f0ed844476ab28563e4e
Eduardo970245 <arizmendie574@gmail.com>
2026-07-11 10:31:57 -0600

    Seguimiento de la estructura

---
+++ middleware/verificarToken.js
@@ -0,0 +1,14 @@
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+module.exports = function verificarToken(req, res, next) {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+  if (!token) return res.status(401).json({ error: 'Token no proporcionado' });
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+    if (err) return res.status(403).json({ error: 'Token inválido o expirado' });
+    req.usuario = decoded;
+    next();
+  });
+};
